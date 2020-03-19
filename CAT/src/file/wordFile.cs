5917cd60c0d17ff5437a2f61ce0047a5bf952696
Nguyen Thanh Long <ntlong281098@gmail.com>
2020-03-19 17:28:51 +0700

    Commit project

---
+++ CAT/src/file/wordFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src.Files
+{
+    public class wordFile : file
+    {
+        public override void readContent(string path)
+        {
+            throw new NotImplementedException();
+        }
+        public override void loadFileSave(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void createFileTranslateDocument()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ CAT/src/segment/Segment.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using src.TM; 
+
+namespace src.segment
+{
+    public class Segment
+    {
+        private tm TM;
+        
+        public void setTM(tm tmp)
+        {
+            TM = tmp; 
+        }
+
+        public void setTMSourceLang(string Lang)
+        {
+            TM.Source = Lang; 
+        }
+
+        public void setTMTargetLang(string Lang)
+        {
+            TM.Target = Lang; 
+        }
+
+        public tm getTM()
+        {
+            return TM; 
+        }
+
+        public string getTMSource()
+        {
+            return TM.Source; 
+        }
+        
+        public string getTMTarget()
+        {
+            return TM.Target; 
+        }
+
+    }
+}
